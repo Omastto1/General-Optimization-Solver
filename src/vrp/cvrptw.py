5ade0d746a4218c4e5947cc31b93b7c07120ecb3
AManWithPlan <michal.lukes3@gmail.com>
2023-05-24 02:43:06 +0200

    Fixed validate and save result

--- src/vrp/cvrptw.py
+++ src/vrp/cvrptw.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import math
+import json
 import matplotlib.pyplot as plt
 from collections import namedtuple
 from docplex.cp.model import CpoModel, CpoParameters
@@ -12,7 +13,7 @@ solver_version = solver.get_version_info()['SolverVersion']
 if compare_natural(solver_version, '22.1.1.0') < 0:
     print('Warning solver version', solver_version, 'is too old for', __file__)
     exit(0)
-    
+
 TIME_FACTOR = 10
 
 
@@ -75,13 +76,17 @@ class CVRPTWProblem:
         self.nb_customers = idx + 1
         self._xy = [self.depot_xy] + self.customers_xy
 
-    def get_num_nodes(self): return self.nb_customers + 1
+    def get_num_nodes(self):
+        return self.nb_customers + 1
 
-    def get_nb_trucks(self): return self.nb_trucks
+    def get_nb_trucks(self):
+        return self.nb_trucks
 
-    def get_capacity(self): return self.truck_capacity
+    def get_capacity(self):
+        return self.truck_capacity
 
-    def get_max_horizon(self): return TIME_FACTOR * self.max_horizon
+    def get_max_horizon(self):
+        return TIME_FACTOR * self.max_horizon
 
     def get_demand(self, i):
         assert i >= 0
@@ -124,9 +129,41 @@ class CVRPTWProblem:
         assert to_ < self.get_num_nodes()
         return self._get_distance(from_, to_)
 
+    def save_to_json(self, file_path):
+        data = {
+            'nb_trucks': self.nb_trucks,
+            'truck_capacity': self.truck_capacity,
+            'max_horizon': self.max_horizon,
+            'nb_customers': self.nb_customers,
+            'depot_xy': self.depot_xy,
+            'customers_xy': self.customers_xy,
+            'demands': self.demands,
+            'earliest_start': self.earliest_start,
+            'latest_start': self.latest_start,
+            'service_time': self.service_time,
+            '_xy': self._xy
+        }
+        with open(file_path, 'w') as f:
+            json.dump(data, f)
+
+    def load_from_json(self, file_path):    # TODO: info about data
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        self.nb_trucks = data['nb_trucks']
+        self.truck_capacity = data['truck_capacity']
+        self.max_horizon = data['max_horizon']
+        self.nb_customers = data['nb_customers']
+        self.depot_xy = data['depot_xy']
+        self.customers_xy = data['customers_xy']
+        self.demands = data['demands']
+        self.earliest_start = data['earliest_start']
+        self.latest_start = data['latest_start']
+        self.service_time = data['service_time']
+        self._xy = data['_xy']
 
 class VRP:
     VisitData = namedtuple("CustomerData", "demand service_time earliest, latest")
+
     def __init__(self, pb):
         # Sizes
         self._num_veh = pb.get_nb_trucks()
@@ -147,31 +184,48 @@ class VRP:
 
         # Visit data
         self._visit_data = \
-            tuple(VRP.VisitData(pb.get_demand(pnode[c]), pb.get_service_time(pnode[c]), pb.get_earliest_start(pnode[c]), pb.get_latest_start(pnode[c])) for c in self._cust) + \
+            tuple(VRP.VisitData(pb.get_demand(pnode[c]), pb.get_service_time(pnode[c]), pb.get_earliest_start(pnode[c]),
+                                pb.get_latest_start(pnode[c])) for c in self._cust) + \
             tuple(VRP.VisitData(0, 0, 0, self._max_horizon) for _ in self._first + self._last)
 
         # Distance
         self._distance = [
-          [ pb.get_distance(pnode[i], pnode[j]) for j in range(self._n) ]
-          for i in range(self._n)
+            [pb.get_distance(pnode[i], pnode[j]) for j in range(self._n)]
+            for i in range(self._n)
         ]
 
-    def first(self) : return self._first
-    def last(self) : return self._last
-    def vehicles(self) : return zip(range(self._num_veh), self._first, self._last)
-    def customers(self) : return self._cust
-    def all(self) : return range(self._n)
+    def first(self): return self._first
+
+    def last(self): return self._last
+
+    def vehicles(self): return zip(range(self._num_veh), self._first, self._last)
+
+    def customers(self): return self._cust
+
+    def all(self): return range(self._n)
+
     def get_num_customers(self): return self._num_cust
+
     def get_num_visits(self): return self._n
+
     def get_num_vehicles(self): return self._num_veh
+
     def get_first(self, veh): return self._first[veh]
+
     def get_last(self, veh): return self._last[veh]
+
     def get_capacity(self): return self._capacity
+
     def get_max_horizon(self): return self._max_horizon
+
     def get_demand(self, i): return self._visit_data[i].demand
+
     def get_service_time(self, i): return self._visit_data[i].service_time
+
     def get_earliest_start(self, i): return self._visit_data[i].earliest
+
     def get_latest_start(self, i): return self._visit_data[i].latest
+
     def get_distance(self, i, j): return self._distance[i][j]
 
 
@@ -195,7 +249,7 @@ def build_model(cvrp_prob, tlim):
 
     # Prev variables, circuit, first/last
     prev = [mdl.integer_var(0, n - 1, "P{}".format(i)) for i in range(n)]
-    for v,fv,lv in vrp.vehicles():
+    for v, fv, lv in vrp.vehicles():
         mdl.add(prev[fv] == vrp.get_last((v - 1) % num_vehicles))
 
     before = vrp.customers() + vrp.first()
@@ -203,14 +257,14 @@ def build_model(cvrp_prob, tlim):
         mdl.add(mdl.allowed_assignments(prev[c], before))
         mdl.add(prev[c] != c)
 
-    for _,fv,lv in vrp.vehicles():
+    for _, fv, lv in vrp.vehicles():
         mdl.add(mdl.allowed_assignments(prev[lv], vrp.customers() + (fv,)))
 
     mdl.add(mdl.sub_circuit(prev))
 
     # Vehicle
-    veh = [ mdl.integer_var(0, num_vehicles - 1, "V{}".format(i)) for i in range(n) ]
-    for v,fv,lv in vrp.vehicles():
+    veh = [mdl.integer_var(0, num_vehicles - 1, "V{}".format(i)) for i in range(n)]
+    for v, fv, lv in vrp.vehicles():
         mdl.add(veh[fv] == v)
         mdl.add(veh[lv] == v)
         mdl.add(mdl.element(veh, prev[lv]) == v)
@@ -218,30 +272,32 @@ def build_model(cvrp_prob, tlim):
         mdl.add(veh[c] == mdl.element(veh, prev[c]))
 
     # Demand
-    load = [ mdl.integer_var(0, vrp.get_capacity(), "L{}".format(i)) for i in range(num_vehicles) ]
+    load = [mdl.integer_var(0, vrp.get_capacity(), "L{}".format(i)) for i in range(num_vehicles)]
     used = mdl.integer_var(0, num_vehicles, 'U')
-    cust_veh = [ veh[c] for c in vrp.customers() ]
-    demand = [ vrp.get_demand(c) for c in vrp.customers() ]
+    cust_veh = [veh[c] for c in vrp.customers()]
+    demand = [vrp.get_demand(c) for c in vrp.customers()]
     mdl.add(mdl.pack(load, cust_veh, demand, used))
 
     # Time
-    start_time = [ mdl.integer_var(vrp.get_earliest_start(i), vrp.get_latest_start(i), "T{}".format(i)) for i in range(n) ]
+    start_time = [mdl.integer_var(vrp.get_earliest_start(i), vrp.get_latest_start(i), "T{}".format(i)) for i in
+                  range(n)]
     for fv in vrp.first():
         mdl.add(start_time[fv] == 0)
     for i in vrp.customers() + vrp.last():
-        arrive = mdl.element([start_time[j] + vrp.get_service_time(j) + vrp.get_distance(j, i) for j in range(n)], prev[i])
+        arrive = mdl.element([start_time[j] + vrp.get_service_time(j) + vrp.get_distance(j, i) for j in range(n)],
+                             prev[i])
         mdl.add(start_time[i] == mdl.max(arrive, vrp.get_earliest_start(i)))
 
     # Distance
     all_dist = []
     for i in vrp.customers() + vrp.last():
-        ldist = [ vrp.get_distance(j, i) for j in range(n) ]
+        ldist = [vrp.get_distance(j, i) for j in range(n)]
         all_dist.append(mdl.element(ldist, prev[i]))
     total_distance = mdl.sum(all_dist) / TIME_FACTOR
 
     # Variables with inferred values
     mdl.add(mdl.inferred(cust_veh + load + [used] + start_time))
-    
+
     # Objective
     mdl.add(mdl.minimize(total_distance))
 
@@ -271,7 +327,7 @@ def display_solution(sol, data):
     vrp = data.vrp
     sprev = tuple(sol.solution[p] for p in data.prev)
 
-    for v,fv,lv in vrp.vehicles():
+    for v, fv, lv in vrp.vehicles():
         route = []
         nd = lv
         while nd != fv:
@@ -279,8 +335,8 @@ def display_solution(sol, data):
             nd = sprev[nd]
         route.append(fv)
         route.reverse()
-        print('Veh {} --->'.format(v, route), end="")
         if len(route) > 2:
+            print('Veh {} --->'.format(v, route), end="")
             arrive = 0
             total_distance = 0
             total_load = 0
@@ -289,7 +345,7 @@ def display_solution(sol, data):
                 early = vrp.get_earliest_start(nd)
                 late = vrp.get_latest_start(nd)
                 start = max(arrive, early)
-                assert(start == sol.solution[data.start_time[nd]])
+                assert (start == sol.solution[data.start_time[nd]])
                 line += " {} (a = {}, t = {} <= {} <= {})".format(nd, arrive, early, start, late)
                 if nd != route[-1]:
                     nxt = route[idx + 1]
@@ -302,8 +358,6 @@ def display_solution(sol, data):
                         total_load += data.vrp.get_demand(nd)
             line += " --- D = {:.1f}, L = {}".format(total_distance, total_load)
             print(line)
-        else:
-            print(" empty")
 
 
 def visualize_solution(sol, data, cvrptw_prob):
@@ -330,9 +384,9 @@ def visualize_solution(sol, data, cvrptw_prob):
         route.append(fv)
 
         if len(route) > 2:
-            print(route)
+            # print(route)
             route = [0 if i + 1 >= len(loc_x) else i + 1 for i in route]
-            print(route)
+            # print(route)
             for i in range(len(route) - 1):
                 plt.plot([loc_x[route[i]], loc_x[route[i + 1]]], [loc_y[route[i]], loc_y[route[i + 1]]], c=colors[c],
                          alpha=0.3)
@@ -347,7 +401,8 @@ def validate_solution(sol, data):
     vrp = data.vrp
     sprev = tuple(sol.solution[p] for p in data.prev)
 
-    for v,fv,lv in vrp.vehicles():
+    total_distance = 0
+    for v, fv, lv in vrp.vehicles():
         route = []
         nd = lv
         while nd != fv:
@@ -359,13 +414,13 @@ def validate_solution(sol, data):
         visited = [False for i in range(vrp.get_num_customers())]
 
         # route = [-1 if i >= vrp.get_num_customers() else i for i in route]
-        assert route[0] >= vrp.get_num_customers() and route[-1] >= vrp.get_num_customers(), f"Vehicle {v} does not start and end at the depot"
+        assert route[0] >= vrp.get_num_customers() and route[
+            -1] >= vrp.get_num_customers(), f"Vehicle {v} does not start and end at the depot"
         if len(route) > 2:
             arrive = 0
-            total_distance = 0
             total_load = 0
             for idx, nd in enumerate(route):
-                if nd < vrp.get_num_customers():    #  Depo doesn't need this check
+                if nd < vrp.get_num_customers():  # Depo doesn't need this check
                     assert visited[nd] == False, f"Customer {nd} has already been been visited"
                     visited[nd] = True
                 early = vrp.get_earliest_start(nd)
@@ -383,11 +438,35 @@ def validate_solution(sol, data):
                     if nd != route[0]:
                         total_load += data.vrp.get_demand(nd)
             assert total_load <= vrp.get_capacity(), f"Vehicle {v} exceeds its capacity"
-    print("Valid solution")
+
+    total_distance /= TIME_FACTOR
+    target = sol.solution.objective_values[0]
+    diff = abs(total_distance - target)
+    assert diff < 0.1, f"Total distance {total_distance} does not match objective value {target}"
+    print("Valid solution, total_distance =", total_distance)
+
+
+def save_solution(sol, path_to_instance):
+    # save solution to json with name, solution and time
+
+    # get instance name
+    instance_name = path_to_instance.split('\\')[-1].split('.')[0]
+    # get solution
+    solution_value = sol.solution.objective_values[0]
+    # TODO: add route
+    # get time
+    time = sol.solver_infos['TotalTime']
+    # create dictionary
+    solution_dict = {'instance_name': instance_name, 'solution': solution_value, 'time': time}
+    # save to json
+    path = '\\'.join(path_to_instance.split('\\')[0:-1])
+    print(f'Saving solution for {instance_name} to {path}\\{instance_name}_result.json')
+    with open(f'{path}\\{instance_name}_result.json', 'w') as f:
+        json.dump(solution_dict, f)
 
 
 if __name__ == "__main__":
-    fname = os.path.dirname(os.path.abspath(__file__)) + "\\..\\..\\data\\solomon_25\\C101.txt"
+    fname = os.path.dirname(os.path.abspath(__file__)) + "\\..\\..\\data\\VRPTW\\solomon_25\\C101.txt"
     if len(sys.argv) != 1:
         if len(sys.argv) != 2:
             print(f'Usage: {sys.argv[0]} OR {sys.argv[0]} <filename>')
@@ -395,14 +474,15 @@ if __name__ == "__main__":
         else:
             fname = sys.argv[1]
 
-    tlim = 15   # TODO: Change for evaluation
+    tlim = 5  # TODO: Change for evaluation
     if len(sys.argv) == 3:
-      tlim = float(sys.argv[2])
+        tlim = float(sys.argv[2])
     cvrptw_prob = CVRPTWProblem()
     cvrptw_prob.read(fname)
     model, data_model = build_model(cvrptw_prob, tlim)
     solution = model.solve()
     if solution:
-        display_solution(solution, data_model)
-        visualize_solution(solution, data_model, cvrptw_prob)
+        # display_solution(solution, data_model)
+        # visualize_solution(solution, data_model, cvrptw_prob)
         validate_solution(solution, data_model)
+        save_solution(solution, fname)
