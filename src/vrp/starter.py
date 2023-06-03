2e717d13d91ad3115dac99352984b700a9c9eb2b
Michal Lukeš <lukesmi1@fel.cvut.cz>
2023-06-03 19:25:11 +0200

    Idk

--- src/vrp/starter.py
+++ src/vrp/starter.py
@@ -6,41 +6,48 @@ def main(args):
     if len(args) != 3:
         print('Usage: python starter.py <data_folder> <output_folder> <number of available cores>')
         return
-    data_folder = args[0]
-    cores = args[3]
+    folder_path = args[0]
+    cores = int(args[2])
 
-    for folder_name in os.listdir(data_folder):
-        if not os.path.isdir(os.path.join(data_folder, folder_name)):
+    # for folder_name in os.listdir(data_folder):
+    if not os.path.isdir(folder_path):
+        print('Usage: python starter.py <data_folder> <output_folder> <number of available cores>')
+        return
+    folder_name = os.path.basename(folder_path)
+    # N = int(folder_name.split('_')[-1])  # Extract the value of N from the folder name
+    # folder_path = os.path.join(data_folder, folder_name)
+    print('folder_name:', folder_name)
+    for filename in os.listdir(folder_path):
+        if not filename.endswith('.json'):
             continue
-        N = int(folder_name.split('_')[-1])  # Extract the value of N from the folder name
-        folder_path = os.path.join(data_folder, folder_name)
-        print('folder_name:', folder_name)
-        for filename in os.listdir(folder_path):
-            if not filename.endswith('.json'):
-                continue
-            instance_name = filename.split('.')[0]  # Extract the instance name from the file name
-            instance_path = os.path.join(folder_path, filename)
+        # folder_name = 'solomon_25'
+        # filename = 'R112.json'
+        instance_name = filename.split('.')[0]  # Extract the instance name from the file name
+        instance_path = os.path.join(folder_path, filename)
 
-            tlim = 60*1  # 15 minutes
+        tlim = 10*1
 
-            print('instance_name:', instance_name)
+        print('instance_name:', instance_name)
 
-            instance = Cvrptw()
-            instance.read_json(instance_path)
+        instance = Cvrptw()
+        instance.read_json(instance_path)
 
+        if instance.instance['best_known_solution'] is not None:
             print('best_known_solution:', instance.instance['best_known_solution']['Distance'])
+        else:
+            print('best_known_solution: None')
 
-            instance.solve(tlim, workers=cores)
+        instance.solve(tlim, workers=cores)
 
-            print('solution:', instance.solution['total_distance'])
+        print('solution:', instance.solution['total_distance'])
 
-            output = os.path.join(args[1], folder_name, instance_name + '.json')
-            if not os.path.exists(os.path.dirname(output)):
-                os.makedirs(os.path.dirname(output))
-            print('Saving to', output)
-            instance.save_to_json(output)
+        output = os.path.join(args[1], folder_name, instance_name + '.json')
+        if not os.path.exists(os.path.dirname(output)):
+            os.makedirs(os.path.dirname(output))
+        print('Saving to', output)
+        instance.save_to_json(output)
 
-            return
+        return
 
 
 if __name__ == "__main__":
