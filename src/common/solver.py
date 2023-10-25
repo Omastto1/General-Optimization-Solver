f2bb9f8a11b1b95918941cf3f490240ffff19dc1
Tomas Omasta <omastto1@fel.cvut.cz>
2023-10-25 12:25:56 +0200

    update abstract solver  base class

--- src/common/solver.py
+++ src/common/solver.py
@@ -1,6 +1,14 @@
 from docplex.cp.model import CpoParameters
+from abc import ABC, abstractmethod
 
-class Solver:
+class Solver(ABC):
+
+    @abstractmethod
+    def solve(self):
+        pass
+
+
+class CPSolver:
     def __init__(self, TimeLimit=60, no_workers=0):
         self.solved = False
         # self.TimeLimit = TimeLimit
@@ -15,21 +23,28 @@ class Solver:
 
         print(f"Time limit set to {TimeLimit} seconds" if TimeLimit is not None else "Time limit not restricted")
 
-    def solve_cp(self, instance, validate=False, visualize=False, force_execution=False):
-        raise ValueError("CP solver not supported for {instance._instance_kind}.")
-    
-    def solve_gp(self, instance, validate=False, visualize=False, force_execution=False):
-        raise ValueError("GP solver not yet implemented.")
-        # raise ValueError("GP solver not supported for {instance._instance_kind}.")
-    
-    def solve(self, instance, method="CP", validate=False, visualize=False, force_execution=False):
-        if method == "CP":
-            print("Running CP solver")
-            solution, xs = self._solve_cp(instance, validate, visualize, force_execution)
-        elif method == "GP":
-            print("Running GP solver")
-            solution, xs = self._solve_gp(instance, validate, visualize, force_execution)
-        else:
-            raise Exception("Method not recognized.")
-        
-        return solution, xs
+    @abstractmethod
+    def solve(self):
+        """Abstract solve method for CP solver."""
+        pass
+
+
+class GPSolver(Solver):
+    def __init__(self, TimeLimit=60, no_workers=0):
+        self.solved = False
+        # self.TimeLimit = TimeLimit
+        self.params = CpoParameters()
+        # params.SearchType = 'Restart'
+        # self.params.LogPeriod = 100000
+        self.params.LogVerbosity = 'Terse'
+        self.params.TimeLimit = TimeLimit
+
+        if no_workers > 0:
+            self.params.Workers = no_workers
+
+        print(f"Time limit set to {TimeLimit} seconds" if TimeLimit is not None else "Time limit not restricted")
+
+    @abstractmethod
+    def solve(self):
+        """Abstract solve method for GP solver."""
+        pass
