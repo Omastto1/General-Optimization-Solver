868dd9e899df84146d99956638a95823274fcd1d
Tomas Omasta <omastto1@fel.cvut.cz>
2024-01-04 15:56:17 +0100

    simplify CP 2d strip packing oriented solver

--- src/strippacking2d/solvers/solver_cp_oriented.py
+++ src/strippacking2d/solvers/solver_cp_oriented.py
@@ -16,54 +16,38 @@ class StripPacking2DCPSolver(CPSolver):
         X = [model.interval_var(name=f'rectangle_{i}_X', start=(0, instance.strip_width - instance.rectangles[i]['width']), size=instance.rectangles[i]['width'], optional=True) for i in range(instance.no_elements)]
         X_rot = [model.interval_var(name=f'rectangle_{i}_Xrot', start=(0, instance.strip_width - instance.rectangles[i]['height'] if instance.strip_width - instance.rectangles[i]['height'] >= 0 else 2 ** 31), size=instance.rectangles[i]['height'], optional=True) for i in range(instance.no_elements)]
         
-        print("1")
         Y_main = [model.interval_var() for i in range(instance.no_elements)]
         Y = [model.interval_var(name=f'rectangle_{i}_Y', size=instance.rectangles[i]['height'], optional=True) for i in range(instance.no_elements)]
         Y_rot = [model.interval_var(name=f'rectangle_{i}_Yrot', size=instance.rectangles[i]['width'], optional=True) for i in range(instance.no_elements)]
 
-        print("2")
         # Orientation decision variables
         O = [model.binary_var() for i in range(instance.no_elements)]
 
         # Adjust size and domain of interval variables based on orientation
         for i in range(instance.no_elements):
-            print(i)
+            # connect main variable with both orientations so that X main is scheduled (copy of one or rotation variable)
             model.add(model.alternative(X_main[i], [X[i], X_rot[i]]))
             model.add(model.alternative(Y_main[i], [Y[i], Y_rot[i]]))
 
+            # Define that only rotated variables or not rotated variables for a specific rectangle cna be used
             model.add(model.if_then(O[i] == 0, model.all_of([model.presence_of(X_rot[i]), model.presence_of(Y_rot[i])])))
             model.add(model.if_then(O[i] == 1, model.all_of([model.presence_of(X[i]), model.presence_of(Y[i])])))
 
-        def double_no_overlap(model, X1, X2, Y1, Y2):
-            # start before end
-            no_overlap_X1 = model.end_of(X1) <= model.start_of(X2)
-            no_overlap_X2 = model.end_of(X2) <= model.start_of(X1)
-            no_overlap_Y1 = model.end_of(Y1) <= model.start_of(Y2)
-            no_overlap_Y2 = model.end_of(Y2) <= model.start_of(Y1)
-
-            # Ensuring that at least one non-overlapping condition is satisfied
-            model.add((no_overlap_X1 | no_overlap_X2) | (no_overlap_Y1 | no_overlap_Y2))
-
-        # Add non-overlap constraints considering both orientations
+        # Add non-overlap constraints
         for i in range(instance.no_elements):
-            print(i)
             for j in range(i + 1, instance.no_elements):
-                double_no_overlap(model, X[i], X[j], Y[i], Y[j])
-                double_no_overlap(model, X_rot[i], X[j], Y_rot[i], Y[j])
-                double_no_overlap(model, X[i], X_rot[j], Y[i], Y_rot[j])
-                double_no_overlap(model, X_rot[i], X_rot[j], Y_rot[i], Y_rot[j])
-
-        print("3")
-        # Create variable z for the total height of the packing
-        z = model.integer_var(0, sum(max(rect.values()) for rect in instance.rectangles))
-
-        print("4")
-        # Add constraints linking z to the Y variables
-        for i in range(instance.no_elements):
-            model.add(model.end_of(Y_main[i]) <= z)
-
+                no_overlap_X1 = model.end_of(X_main[i]) <= model.start_of(X_main[j])
+                no_overlap_X2 = model.end_of(X_main[j]) <= model.start_of(X_main[i])
+                no_overlap_Y1 = model.end_of(Y_main[i]) <= model.start_of(Y_main[j])
+                no_overlap_Y2 = model.end_of(Y_main[j]) <= model.start_of(Y_main[i])
+                
+                # Ensuring that at least one non-overlapping condition is satisfied
+                model.add((no_overlap_X1 | no_overlap_X2) | (no_overlap_Y1 | no_overlap_Y2))
+            
         # Minimize the total height
-        model.add(model.minimize(z))
+        model.add(model.minimize(model.max([model.end_of(Y_main[i])
+                                            for i in range(instance.no_elements)])))  # (1)
+
 
         return model, X, Y, X_rot, Y_rot
     
