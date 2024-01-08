a1cb8062942784762bd7f9369ab4bad2adf4f5c8
Tomas Omasta <omastto1@fel.cvut.cz>
2024-01-08 20:20:09 +0100

    clean up rcpsp

--- src/rcpsp/solvers/solver_cp.py
+++ src/rcpsp/solvers/solver_cp.py
@@ -4,7 +4,7 @@ from src.common.solver import CPSolver
 
 
 class RCPSPCPSolver(CPSolver):
-    solver_name="CP Default"
+    solver_name = "CP Default"
 
     def build_model(self, instance, initial_solution=None):
         model = CpoModel()
@@ -12,15 +12,16 @@ class RCPSPCPSolver(CPSolver):
 
         x = [model.interval_var(size=duration, name=f"{i}") for i, duration in enumerate(
             instance.durations)]  # (4)
-        
+
         if initial_solution is not None:
             self.solver_name += " Hybrid"
-            
+
             stp = model.create_empty_solution()
 
             for i, duration in enumerate(instance.durations):
-                stp.add_interval_var_solution(x[i], start=initial_solution[i], end=initial_solution[i] + duration)
-                
+                stp.add_interval_var_solution(
+                    x[i], start=initial_solution[i], end=initial_solution[i] + duration)
+
             model.set_starting_point(stp)
 
         cost = model.integer_var(0, 1000000, name="cost")
@@ -39,9 +40,9 @@ class RCPSPCPSolver(CPSolver):
 
         model.add([model.end_before_start(x[i], x[successor - 1]) for (i, job_successors)
                    in enumerate(instance.successors) for successor in job_successors])  # (3)
-        
+
         return model, {"x": x}
-    
+
     def _export_solution(self, instance, sol, model_variables):
         x = model_variables['x']
 
@@ -52,11 +53,11 @@ class RCPSPCPSolver(CPSolver):
             end = interval_value.end
 
             export.append(
-                {"start": start, 
-                "end": end,
-                "name": x[i].name}
-                )
-        
+                {"start": start,
+                 "end": end,
+                 "name": x[i].name}
+            )
+
         return {"tasks_schedule": export}
 
     def _solve(self, instance, validate=False, visualize=False, force_execution=False, initial_solution=None, update_history=True):
@@ -69,13 +70,15 @@ class RCPSPCPSolver(CPSolver):
         if sol.get_solve_status() in ["Unknown", "Infeasible", "JobFailed", "JobAborted"]:
             print('No solution found')
             return None, None, sol
-        
-        model_variables_export = self._export_solution(instance, sol, model_variables)
+
+        model_variables_export = self._export_solution(
+            instance, sol, model_variables)
 
         if validate:
             try:
                 print("Validating solution...")
-                is_valid = instance.validate(None, None, model_variables_export)
+                is_valid = instance.validate(
+                    None, None, model_variables_export)
                 if is_valid:
                     print("Solution is valid.")
                 else:
@@ -99,24 +102,16 @@ class RCPSPCPSolver(CPSolver):
             print("Unknown solution status")
             print(sol.get_solve_status())
 
-        instance.compare_to_reference(obj_value)
-        # for i in range(no_jobs):
-        #     print(f"Activity {i}: start={sol[i].get_start()}, end={sol[i].get_end()}")
-
         print(sol.solution.get_objective_bounds())
         print(sol.solution.get_objective_gaps())
         print(sol.solution.get_objective_values())
 
-        # obj_value = sol.objective_value
         obj_value = sol.get_objective_values()[0]
         print('Objective value:', obj_value)
-        # start_times = [sol.get_var_solution(x[i]).get_start() for i in range(instance.no_jobs)]
         instance.compare_to_reference(obj_value)
-
 
         self.add_run_to_history(instance, sol)
 
-        # print solution
         if sol.get_solve_status() == 'Optimal':
             print("Optimal solution found")
         elif sol.get_solve_status() == 'Feasible':
@@ -129,5 +124,5 @@ class RCPSPCPSolver(CPSolver):
 
         if update_history:
             self.add_run_to_history(instance, sol)
-        
+
         return obj_value, model_variables_export, sol
--- src/rcpsp/solvers/solver_ga.py
+++ src/rcpsp/solvers/solver_ga.py
@@ -3,12 +3,14 @@ from pymoo.core.problem import ElementwiseProblem
 from pymoo.optimize import minimize
 from src.common.solver import GASolver
 
+
 class RCPSPGASolver(GASolver):
     def _solve(self, instance, validate=False, visualize=False, force_execution=False, update_history=True):
         class RCPSP(ElementwiseProblem):
-            
+
             def __init__(self, instance, fitness_func):
-                super().__init__(n_var=instance.no_jobs, n_obj=1, n_constr=instance.no_renewable_resources, xu=100, xl=0)
+                super().__init__(n_var=instance.no_jobs, n_obj=1,
+                                 n_constr=instance.no_renewable_resources, xu=100, xl=0)
                 self.instance = instance
                 self.fitness_func = fitness_func
 
@@ -18,7 +20,7 @@ class RCPSPGASolver(GASolver):
                 assert "solution" not in out, "Do not use `solution` key, it is pymoo reserved keyword"
 
                 return out
-        
+
         if not force_execution and len(instance._run_history) > 0:
             if instance.skip_on_optimal_solution():
                 return None, None
@@ -35,7 +37,8 @@ class RCPSPGASolver(GASolver):
             d = {}
             problem._evaluate(X, d)
             start_times = d['start_times']
-            export = {"tasks_schedule": [{"start": start_times[i], "end": start_times[i] + instance.durations[i], "name": f"Task_{i}"} for i in range(instance.no_jobs)]}
+            export = {"tasks_schedule": [{"start": start_times[i], "end": start_times[i] +
+                                          instance.durations[i], "name": f"Task_{i}"} for i in range(instance.no_jobs)]}
 
             if validate:
                 try:
@@ -43,17 +46,12 @@ class RCPSPGASolver(GASolver):
                     instance.validate(None, None, export)
                     print("Solution is valid.")
 
-                    # TODO
-                    # obj_value = sol.get_objective_value()
-                    # print("Project completion time:", obj_value)
-
-                    # TODO
-                    # instance.compare_to_reference(obj_value)
                 except AssertionError as e:
                     print("Solution is invalid.")
                     print(e)
 
-                    self.add_run_to_history(instance, fitness_value, start_times, is_valid=False)
+                    self.add_run_to_history(
+                        instance, fitness_value, start_times, is_valid=False)
 
                     return None, None, res
 
@@ -66,7 +64,8 @@ class RCPSPGASolver(GASolver):
         if update_history:
             solution_info = f"start_times: {start_times}"
             solution_progress = res.algorithm.callback.data['progress']
-            self.add_run_to_history(instance, fitness_value, solution_info, solution_progress, exec_time=round(res.exec_time, 2))
+            self.add_run_to_history(instance, fitness_value, solution_info,
+                                    solution_progress, exec_time=round(res.exec_time, 2))
 
         if res.F is not None:
             return fitness_value, start_times, res
